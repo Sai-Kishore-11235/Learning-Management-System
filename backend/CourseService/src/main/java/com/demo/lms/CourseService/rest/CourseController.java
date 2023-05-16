b42159c12d89f201e7dff8637ebb7d13856fe558
sai kishore <sai.kishore.11235@gmail.com>
2023-05-16 19:46:21 +0530

    frontend added

--- backend/CourseService/src/main/java/com/demo/lms/CourseService/rest/CourseController.java
+++ backend/CourseService/src/main/java/com/demo/lms/CourseService/rest/CourseController.java
@@ -1,13 +1,13 @@
 package com.demo.lms.CourseService.rest;
 
 import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/v1.0/lms/courses")
-public class CourseController {
-
-    @PostMapping("/add/{courseName}")
-    public String createCourse(@PathVariable String courseName){
-        return "hello"+courseName;
-    }
-}
+//
+//@RestController
+//@RequestMapping("/api/v1.0/lms/courses")
+//public class CourseController {
+//
+//    @PostMapping("/add/{courseName}")
+//    public String createCourse(@PathVariable String courseName){
+//        return "hello"+courseName;
+//    }
+//}
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { RegisterComponent } from './components/register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+// import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; 
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LogInComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    NgbModule
+  ],
+  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
