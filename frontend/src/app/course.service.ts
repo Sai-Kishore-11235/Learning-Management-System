f452f64516f79424372d5beb9c271bf66b0d7793
sai kishore <sai.kishore.11235@gmail.com>
2023-06-19 19:53:20 +0530

    changes to docker

--- frontend/src/app/course.service.ts
+++ frontend/src/app/course.service.ts
@@ -12,33 +12,33 @@ export class CourseService {
   getAllCourses():Observable<any>{
     let headers = new HttpHeaders({'Content-Type': 'application/json'})
     // headers.append('Access-Control-Allow-Origin', '*')
-    const url = "http://localhost:8082/course-service/api/v1.0/lms/courses/getall"
+    const url = "http://13.232.144.85:8082/course-service/api/v1.0/lms/courses/getall"
     return this.http.get(url,{headers})
   }
 
   saveCourse(course:any):Observable<any>{
     let headers = new HttpHeaders({'Content-Type': 'text/plain'})
     // headers.append('Access-Control-Allow-Origin', '*')
-    const url = `http://localhost:8082/course-service/api/v1.0/lms/courses/add/${course.title}`
+    const url = `http://13.232.144.85:8082/course-service/api/v1.0/lms/courses/add/${course.title}`
     return this.http.post(url,course)
   }
 
   deleteCourse(course:any):Observable<any>{
     let headers = new HttpHeaders({'Content-Type': 'text/plain'})
     // headers.append('Access-Control-Allow-Origin', '*')
-    const url = `http://localhost:8082/course-service/api/v1.0/lms/courses/delete/${course.title}`
+    const url = `http://13.232.144.85:8082/course-service/api/v1.0/lms/courses/delete/${course.title}`
     return this.http.delete(url,{headers})
   }
   getByTechnology(technology:any):Observable<any>{
     let headers = new HttpHeaders({'Content-Type': 'text/plain'})
     // headers.append('Access-Control-Allow-Origin', '*')
-    const url = `http://localhost:8082/course-service/api/v1.0/lms/courses/info/${technology}`
+    const url = `http://13.232.144.85:8082/course-service/api/v1.0/lms/courses/info/${technology}`
     return this.http.get(url,{headers})
   }
   getByTechnologyAndMinMaxhours(data:any):Observable<any>{
     let headers = new HttpHeaders({'Content-Type': 'text/plain'})
     // headers.append('Access-Control-Allow-Origin', '*')
-    const url = `http://localhost:8082/course-service/api/v1.0/lms/courses/get/${data.technology}/${data.minHours}/${data.maxHours}`
+    const url = `http://13.232.144.85:8082/course-service/api/v1.0/lms/courses/get/${data.technology}/${data.minHours}/${data.maxHours}`
     return this.http.get(url,{headers})
   }
 }
